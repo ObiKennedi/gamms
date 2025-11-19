17dbcc812e07cb974d762eac71ae11bedda6e2fe
Obi Kennedy <166854306+ObiKennedi@users.noreply.github.com>
2025-11-19 14:12:04 +0100

    Fix scss

---
+++ components/ValueProposition.jsx
@@ -0,0 +1,16 @@
+import { InfinityIcon } from "lucide-react";
+
+const ValuePropsItems = [
+    { id: 1, title: "100%", feature: "Data ownership" },
+    { id: 2, title: "24/7", feature: "Uptime Guaranteed" },
+    { id: 3, title: "100%", feature: "Free and accessible" },
+    { id: 4, title: <InfinityIcon />, feature: "Possibilities" },
+];
+
+export const ValueProps = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
