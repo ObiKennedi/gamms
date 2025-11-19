f152fe1ab9ee34266c01b7735fa8eb8e2ef52281
Obi Kennedy <166854306+ObiKennedi@users.noreply.github.com>
2025-11-19 21:06:58 +0100

    Not done

---
+++ components/Everything.jsx
@@ -0,0 +1,11 @@
+export const Everything = () =>{
+    return(
+        <div>
+            <h2>Everything your community needs  in one place</h2>
+            <p>From seamless management to active engagement, Gamms gives you every tool to build, grow, and sustain a thriving community.</p>
+            <div>
+                <MdGroups/>
+            </div>
+        </div>
+    )
+}
